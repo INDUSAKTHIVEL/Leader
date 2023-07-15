80d8c6d8b416b861f558dec30976da01ebd2cf83
INDUSAKTHIVEL <indusakthi2003@gmail.com>
2023-07-15 21:56:37 +0530

    Leader

--- Leader.java
+++ Leader.java
@@ -1,10 +1,33 @@
 package Leaders_array;
-
+import java.util.Scanner;
 public class Leader {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		Leader led=new Leader();
+		int n=sc.nextInt();
+		int arr[]= new int [n];
+		for (int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		led.printled(arr , n);
 	}
-
+	void printled(int arr[],int a)
+	{
+		int i;
+		for( i=0;i<a;i++)
+		{
+			int j;
+			for( j=i+1;j<a;j++)
+			{
+				if(arr[i] <=arr[j])
+					break;
+			}
+			if(j==a) {
+				System.out.println(arr[i]+" ");
+			}
+		}		
+		
+}
 }
